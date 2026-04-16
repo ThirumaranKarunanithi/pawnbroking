d42bfd87c17b8b0ba9f3b243d4a1f342474731fa
Thirumaran Karunanithi <magizhchisoftware@gmail.com>
2026-04-17 01:27:57 +0530

    first commit

--- PawnBroking/run.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d "%~dp0"
-java --module-path "target/javafx-lib" ^
-     --add-modules javafx.controls,javafx.fxml,javafx.swing,javafx.graphics ^
-     -Dsun.java2d.uiScale=1 ^
-     -cp "target/lib/*;target/PawnBroking-1.0-SNAPSHOT.jar" ^
-     com.magizhchi.pawnbroking.Launcher
-pause
--- PawnBroking/src/main/java/com/magizhchi/pawnbroking/Launcher.java
+++
@@ -1,7 +0,0 @@
-package com.magizhchi.pawnbroking;
-
-public class Launcher {
-    public static void main(String[] args) {
-        PawnBroking.main(args);
-    }
-}
